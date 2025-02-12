b8b0ed1ab502ca05abd0474db2d4207feaa5be12
Davidetat1 <davidetatone92@gmail.com>
2025-02-12 20:34:07 +0100

    fix: update import path for Header component to match file naming convention

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
-import Header from "./sections/header.jsx";
+import Header from "./sections/Header.jsx";
 import Hero from "./sections/Hero.jsx";
 import Projects from "./sections/Projects.jsx";
 import About from "./sections/About.jsx";
 import Skills from "./sections/Skills.jsx";
 import Experiences from "./sections/Experiences.jsx";
-import ShapeDividerHero from "./components/shape/shapeBottom/ShapeBottom.jsx";
 
 const App = () => {
   const [menuOpen, setMenuOpen] = useState();
